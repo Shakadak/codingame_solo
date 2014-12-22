490cf3ecd1c3ebe8346b4207ad27bd17ca6f1a21
Nathanael <Shakadak@users.noreply.github.com>
2014-12-22 12:55:44 +0100

    Final version, lazy version.

--- skynet_jump.hs
+++ skynet_jump.hs
@@ -9,44 +9,32 @@ main = do
     -- the standard input according to the problem statement.
 
     input_line <- getLine
-    hPutStrLn stderr input_line
     let r = read input_line :: Int -- the length of the road before the gap.
     input_line <- getLine
-    hPutStrLn stderr input_line
     let g = read input_line :: Int -- the length of the gap.
     input_line <- getLine
-    hPutStrLn stderr input_line
     let l = read input_line :: Int -- the length of the landing platform.
-    input_line <- getLine
-    --hPutStrLn stderr input_line
-    let speed = read input_line :: Int -- the starting speed of the motorbike.
-    loop
+    loop g r
 
-loop :: IO ()
-loop = do
+loop :: Int -> Int -> IO ()
+loop gap gap_position = do
     input_line <- getLine
-    hPutStrLn stderr input_line
-    hPutStrLn stderr ""
-    let s = read input_line :: Int -- the position on the road of the motorbike.
+    let speed = read input_line :: Int -- the motorbike's speed.
+    input_line <- getLine
+    let bike_position = read input_line :: Int -- the position on the road of the motorbike.
 
     -- hPutStrLn stderr "Debug messages..."
 
     -- A single line containing one of 4 keywords: SPEED, SLOW, JUMP, WAIT.
-    putStrLn "SPEED"
-    input_line <- getLine
-    let x = read input_line :: Int -- the motorbike's speed.
+    putStrLn $ action gap speed gap_position bike_position
 
-    loop
+    loop gap gap_position
 
-stop_distance :: Int -> Int
-stop_distance 0 = 0
-stop_distance speed = speed - 1 + stop_distance (speed - 1)
-
-max_speed_in_platform :: Int -> Int
-max_speed_in_platform platform_size = maximum $ takeWhile (`stop_distance_is_less_than` platform_size) [1,2..]
-
-stop_distance_is_less_than :: Int -> Int -> Bool
-speed `stop_distance_is_less_than` platform_size = stop_distance speed <= platform_size
-
-min_jump_speed :: Int -> Int
-min_jump_speed gap_size = gap_size + 1
+action :: Int -> Int -> Int -> Int -> String
+action _ _ gap_position bike_position
+	|bike_position > gap_position = "SLOW"
+	|bike_position == gap_position - 1 = "JUMP"
+action gap speed _ _
+    |speed < gap + 1 = "SPEED"
+    |speed > gap + 1 = "SLOW"
+	|otherwise = "WAIT"
