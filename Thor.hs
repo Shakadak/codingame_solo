0e4885d839d461057208081854e576dd502741f8
nathanael <nathanael.pineau@gmail.com>
2015-07-30 12:33:37 +0200

    removing some things

--- Thor.hs
+++ Thor.hs
@@ -3,36 +3,28 @@ import Control.Monad
 
 main :: IO ()
 main = do
-    hSetBuffering stdout NoBuffering -- DO NOT REMOVE
-
-    -- Auto-generated code below aims at helping you parse
-    -- the standard input according to the problem statement.
-
-    input_line <- getLine
-    let input = words input_line
-    let lx = read (input!!0) :: Int -- the X position of the light of power
-    let ly = read (input!!1) :: Int -- the Y position of the light of power
-    let tx = read (input!!2) :: Int -- Thor's starting X position
-    let ty = read (input!!3) :: Int -- Thor's starting Y position
-    loop $ readable_path (lx - tx, ly - ty)
+    hSetBuffering stdout NoBuffering
+    il <- getLine
+    let i = words il
+    let lx = read (i!!0) :: Int
+    let ly = read (i!!1) :: Int
+    let tx = read (i!!2) :: Int
+    let ty = read (i!!3) :: Int
+    loop $ readablePath (lx - tx, ly - ty)
 
 loop :: [String] -> IO ()
-loop (step:path) = do
-    input_line <- getLine
-    let _ = read input_line :: Int -- The level of Thor's remaining energy, representing the number of moves he can still make.
-
-    -- A single line providing the move to be made: N NE E SE S SW W or NW
-    putStrLn step
-
-    loop path
 loop [] = return ()
+loop (x:xs) = do
+    getLine
+    putStrLn x
+    loop xs
 
 readablePath :: (Int, Int) -> [String]
 readablePath (0, 0) = []
-readablePath position = readable_step position : readable_path (next_step position)
+readablePath position = readableStep position : readablePath (nextStep position)
 
 readableStep :: (Int, Int) -> String
-readableStep (px, py) = latitude py ++ longitude px
+readableStep (px, py) = lat py ++ long px
 
 next :: Int -> Int
 next 0 = 0
@@ -43,14 +35,14 @@ next n
 nextStep :: (Int, Int) -> (Int, Int)
 nextStep (x, y) = (next x, next y)
 
-longitude :: Int -> String
-longitude l
+long :: Int -> String
+long l
     | l > 0 = "E"
     | l < 0 = "W"
     | otherwise = []
 
-latitude :: Int -> String
-latitude l
+lat :: Int -> String
+lat l
     | l > 0 = "S"
     | l < 0 = "N"
     | otherwise = []
